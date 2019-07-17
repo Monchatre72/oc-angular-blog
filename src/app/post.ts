cfd98e9959a655954a7de7da97c282bb59acc9b2
Benjamin Monchatre <benjamin.monchatre@gmail.com>
2019-07-17 17:19:17 +0200

    svg

---
+++ src/app/post.ts
@@ -0,0 +1,15 @@
+export class Post {
+
+
+  title:string;
+  content:string;
+  creationDate : Date;
+  like : number;
+
+
+  constructor(
+
+  ) {
+   this.creationDate= new Date (),
+   this.like=0}
+}
